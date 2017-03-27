4abb5eb0a7e745dbd25c4490dcc4f7f0993b4327
Kiril Kamburov <kiril@Kirils-Laptop.local>
2017-03-27 14:48:04 +0100

    Add support for au10tix Integration Add SOAPAction support Add debug support Add removal of self-closing tags

--- soap/client.go
+++ soap/client.go
@@ -8,6 +8,9 @@ import (
 	"io"
 	"io/ioutil"
 	"net/http"
+	"net/http/httputil"
+	"regexp"
+	"strings"
 )
 
 // A RoundTripper executes a request passing the given req as the SOAP
@@ -39,9 +42,11 @@ type Client struct {
 	Namespace   string              // SOAP Namespace
 	Envelope    string              // Optional SOAP Envelope
 	Header      Header              // Optional SOAP Header
+	SOAPAction  string              // Optional SOAP Action name
 	ContentType string              // Optional Content-Type (default text/xml)
 	Config      *http.Client        // Optional HTTP client
 	Pre         func(*http.Request) // Optional hook to modify outbound requests
+	Debug       bool                // Optional Print the request and response messages
 }
 
 // RoundTrip implements the RoundTripper interface.
@@ -49,36 +54,51 @@ func (c *Client) RoundTrip(in, out Message) error {
 	req := &Envelope{
 		EnvelopeAttr: c.Envelope,
 		NSAttr:       c.Namespace,
-		Header:       c.Header,
+		Header:       EnvelopeHeader{SOAPAction: c.SOAPAction},
 		Body:         Body{Message: in},
 	}
+
 	if req.EnvelopeAttr == "" {
 		req.EnvelopeAttr = "http://schemas.xmlsoap.org/soap/envelope/"
 	}
-	if req.NSAttr == "" {
-		req.NSAttr = c.URL
-	}
+
 	var b bytes.Buffer
 	err := xml.NewEncoder(&b).Encode(req)
 	if err != nil {
 		return err
 	}
+
 	ct := c.ContentType
 	if ct == "" {
 		ct = "text/xml"
 	}
+
 	cli := c.Config
 	if cli == nil {
 		cli = http.DefaultClient
 	}
+
 	r, err := http.NewRequest("POST", c.URL, &b)
 	if err != nil {
 		return err
 	}
+
 	r.Header.Set("Content-Type", ct)
+
 	if c.Pre != nil {
 		c.Pre(r)
 	}
+
+	if c.Debug {
+		requestDump, err := httputil.DumpRequest(r, true)
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Println("Request start ----")
+		fmt.Println(string(requestDump))
+		fmt.Println("Request end ------")
+	}
+
 	resp, err := cli.Do(r)
 	if err != nil {
 		return err
@@ -90,15 +110,47 @@ func (c *Client) RoundTrip(in, out Message) error {
 		body, _ := ioutil.ReadAll(limReader)
 		return fmt.Errorf("%q: %q", resp.Status, body)
 	}
-	return xml.NewDecoder(resp.Body).Decode(out)
+
+	if c.Debug {
+		responseDump, err := httputil.DumpResponse(resp, true)
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Println("Response start ----")
+		fmt.Println(string(responseDump))
+		fmt.Println("Response end ------")
+	}
+
+	resBodyBuf := new(bytes.Buffer)
+	resBodyBuf.ReadFrom(resp.Body)
+	respBodyString := resBodyBuf.String()
+
+	// extract SOAP envelope from HTTP response body
+	soapEnvelopeRegEx := regexp.MustCompile("<s:Envelope.*s:Envelope>")
+	soapEnvelopeMatches := soapEnvelopeRegEx.FindAllStringSubmatch(respBodyString, -1)
+	// there should be only one soap envelop in the HTTP response body
+	extractedSOAPEnvelope := soapEnvelopeMatches[0][0]
+	if c.Debug {
+		fmt.Println("Extracted SOAP envelope = ", extractedSOAPEnvelope)
+	}
+
+	// remove self closing tags
+	selfClosingTagRegExp := regexp.MustCompile("<\\w:\\w+ i:nil=\"true.*?/>")
+	clearedSOAPEnvelope := selfClosingTagRegExp.ReplaceAllString(extractedSOAPEnvelope, "")
+	if c.Debug {
+		fmt.Println("Cleared SOAP Envelope = ", clearedSOAPEnvelope)
+	}
+
+	decoder := xml.NewDecoder(strings.NewReader(clearedSOAPEnvelope))
+	return decoder.Decode(out)
 }
 
 // Envelope is a SOAP envelope.
 type Envelope struct {
 	XMLName      xml.Name `xml:"SOAP-ENV:Envelope"`
 	EnvelopeAttr string   `xml:"xmlns:SOAP-ENV,attr"`
-	NSAttr       string   `xml:"xmlns:ns,attr"`
-	Header       Message  `xml:"SOAP-ENV:Header"`
+	NSAttr       string   `xml:"xmlns:ns,attr,omitempty"`
+	Header       EnvelopeHeader
 	Body         Body
 }
 
@@ -106,4 +158,10 @@ type Envelope struct {
 type Body struct {
 	XMLName xml.Name `xml:"SOAP-ENV:Body"`
 	Message Message
+}
+
+// EnvelopeHeader is the header of a SOAP envelope.
+type EnvelopeHeader struct {
+	XMLName    xml.Name `xml:"SOAP-ENV:Header"`
+	SOAPAction string `xml:"http://www.w3.org/2005/08/addressing Action"`
 }
